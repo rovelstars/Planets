22cc15d643f21a62fde169f927046f1285e495ed
Ren Hiyama <renhiyama@rovelstars.com>
2026-06-07 00:32:34 +0530

    aether package: build/install aetherctl (renamed from aether CLI)

--- packages/aether/build.sh
+++ packages/aether/build.sh
@@ -19,12 +19,12 @@ build() {
     export CARGO_TARGET_X86_64_ROVELSTARS_LINUX_RUNIXOS_LINKER="$SYSROOT/Core/Bin/clang"
     export CARGO_TARGET_X86_64_ROVELSTARS_LINUX_RUNIXOS_RUSTFLAGS="-C link-arg=--target=x86_64-rovelstars-linux-runixos -C link-arg=--sysroot=$SYSROOT -C link-arg=-fuse-ld=lld"
     export CC_x86_64_rovelstars_linux_runixos="$SYSROOT/Core/Bin/clang"
-    cargo build --release --target "$TARGET" -p aetherd -p aether -j"$JOBS"
+    cargo build --release --target "$TARGET" -p aetherd -p aetherctl -j"$JOBS"
 }
 
 install() {
     cd "$SRC/aether"
     mkdir -p "$OUTPUT/Core/Bin"
     cp "target/$TARGET/release/aetherd" "$OUTPUT/Core/Bin/"
-    cp "target/$TARGET/release/aether" "$OUTPUT/Core/Bin/"
+    cp "target/$TARGET/release/aetherctl" "$OUTPUT/Core/Bin/"
 }
